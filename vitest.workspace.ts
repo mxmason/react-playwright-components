4d4ef17148fc50d875d8b08e24da8aed981d8aab
EJ Mason <13525251+mxmason@users.noreply.github.com>
2025-04-25 16:53:54 -0700

    chore: configure vitest for CI

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,26 +1,36 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-	// If you want to keep running your existing tests in Node.js, uncomment the next line.
-	// 'vite.config.ts',
-	{
-		extends: 'vite.config.ts',
-		test: {
-			browser: {
-				enabled: true,
-				provider: 'playwright',
+  // If you want to keep running your existing tests in Node.js, uncomment the next line.
+  // 'vite.config.ts',
+  {
+    extends: 'vite.config.ts',
+    test: {
+      browser: {
+        enabled: true,
+        provider: 'playwright',
         screenshotFailures: false,
-				// https://vitest.dev/guide/browser/playwright
-				instances: [{ browser: 'chromium' }],
-			},
-			exclude: [
-				'**/node_modules/**',
-				'**/dist/**',
-				'**/cypress/**',
-				'**/.{idea,git,cache,output,temp}/**',
-				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-				'**/*.component.*',
-			],
-		},
-	},
+        // https://vitest.dev/guide/browser/playwright
+        instances: [
+          {
+            browser: 'chromium',
+            // @ts-ignore this is a real option i promise
+            launch: {
+              executablePath: process.env.CI
+                ? process.env.CHROMIUM_BIN
+                : undefined,
+            },
+          },
+        ],
+      },
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+        '**/*.component.*',
+      ],
+    },
+  },
 ]);
